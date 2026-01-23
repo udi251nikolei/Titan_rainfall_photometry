a94c9b5e2d58a3d1f7432edc0d7a381b1041dcbc
Nikolei <113668786+udi251nikolei@users.noreply.github.com>
2026-01-23 12:09:30 +0100

    Update functs.py
    Draw_aperture function works well with NAC CB2 1024 images, but WAC CB2 distance are strangely too large.

--- functs.py
+++ functs.py
@@ -101,24 +101,27 @@ def draw_titan_aperture(image_data, planet_cassini_distance, pixel_angular_size)
     
     cumulate_middle_xaxis = (np.cumsum(xaxis_marginal) <= 0.5)
     middle_bound_xaxis = cumulate_middle_xaxis.sum()
-    print(f"0.5 cumulative bound along xaxis columns: {middle_bound_xaxis}")
+    print(f"50% cumulative bound along xaxis columns: {middle_bound_xaxis}")
     
     cumulate_middle_yaxis = (np.cumsum(yaxis_marginal) <= 0.5)
     middle_bound_yaxis = cumulate_middle_yaxis.sum()
-    print(f"0.5 cumulative bound along yaxis rows: {middle_bound_yaxis}")
+    print(f"50% cumulative bound along yaxis rows: {middle_bound_yaxis}")
     
-    planet_radius = 2975 #2575km
+    planet_radius = 2975 #km
     planet_pixel_radius = np.arctan(planet_radius/planet_cassini_distance) / pixel_angular_size
-    print(f"Planet raidus in pixels: {planet_pixel_radius}\n")
+    print(f"Titan raidus in pixels: {planet_pixel_radius}\n")
+    extra_radius = 0
+    aperture_radius = planet_pixel_radius + extra_radius
+    print(f"Aperture raidus in pixels: {aperture_radius}\n")
     
     #------------------------------------------------------------------
     
-    #aperture = CircularAperture((x_apature, y_apature), r=r_apature)
+    aperture = CircularAperture((middle_bound_xaxis, middle_bound_yaxis), r=aperture_radius)
     
     fig, ax = plt.subplots(figsize=(8, 8))
 
-    ax.imshow(image_data, cmap='gray')
-    #aperture.plot(color = "white", lw = 1.5,  linestyle = "dashed")
+    ax.imshow(image_data, origin="lower", cmap="gray")
+    aperture.plot(color = "blue", lw = 1.5,  linestyle = "dashed")
 
     ax.set_xlabel("CCD's x-axis")
     ax.set_ylabel("CCD's y-axis")
@@ -133,28 +136,25 @@ def draw_titan_aperture(image_data, planet_cassini_distance, pixel_angular_size)
     square_image_range = np.arange(0, image_pixel_length)
 
     ax_histx.plot(square_image_range, xaxis_marginal, color="tab:gray", alpha=0.2)
-    #ax_histx.set_xlim(0, len(xaxis_marginal))
-    #ax_histx.set_ylim(0)
+    ax_histx.set_xlim(0, len(xaxis_marginal))
+    ax_histx.set_ylim(0)
     ax_histx.fill_between(square_image_range, 0, xaxis_marginal, color="tab:gray", alpha=0.5)
     ax_histx.axvline(middle_bound_xaxis, color="black", linestyle="dashed")
-    ax_histx.axvline(middle_bound_xaxis + planet_pixel_radius, color="tab:blue", linestyle="dashed")
-    ax_histx.axvline(middle_bound_xaxis - planet_pixel_radius, color="tab:blue", linestyle="dashed")
+    ax_histx.axvline(middle_bound_xaxis + aperture_radius, color="tab:blue", linestyle="dashed")
+    ax_histx.axvline(middle_bound_xaxis - aperture_radius, color="tab:blue", linestyle="dashed")
     
     ax_histy.plot(yaxis_marginal, square_image_range, color="tab:gray", alpha=0.2)
-    #ax_histy.set_xlim(0)
-    #ax_histy.set_ylim(0, len(yaxis_marginal))
+    ax_histy.set_xlim(0)
+    ax_histy.set_ylim(0, len(yaxis_marginal))
     ax_histy.fill_between(yaxis_marginal, 0, square_image_range, color="tab:gray", alpha=0.5)
     ax_histy.axhline(middle_bound_yaxis, color="black", linestyle="dashed")
-    ax_histy.axhline(middle_bound_yaxis + planet_pixel_radius, color="tab:blue", linestyle="dashed")
-    ax_histy.axhline(middle_bound_yaxis - planet_pixel_radius, color="tab:blue", linestyle="dashed")
+    ax_histy.axhline(middle_bound_yaxis + aperture_radius, color="tab:blue", linestyle="dashed")
+    ax_histy.axhline(middle_bound_yaxis - aperture_radius, color="tab:blue", linestyle="dashed")
     
     plt.show()
     
     return None
     
-    
-    
-
 def get_titan_aperture(image_file, image_number, camera_filter, camera, planet_cassini_distance, pixel_angular_size):
     
     image_data = get_image_data(image_file, camera_filter, camera)
