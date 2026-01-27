8457cea31cb07a21c9f37a26b07d8eced8d6049f
Nikolei <113668786+udi251nikolei@users.noreply.github.com>
2026-01-27 14:13:47 +0100

    Add files via upload

---
+++ Save_CB2_NAC_IMG_as_FITS.py
@@ -0,0 +1,34 @@
+import functs
+import Get_image_info
+
+image_files = Get_image_info.CB2_NAC_image_files #!!! Need to change camera type here; needs to be same as folder name
+image_files_length = len(image_files)
+print(f"Total number of image files: {image_files_length}\n")
+
+camera_filter = "CB2"
+camera = "NAC" #!!! Need to change camera type here; needs to be same as folder name
+
+total_converted_images = 0
+total_failed_conversion = 0
+failed_converted_files = []
+
+for n in range(image_files_length):
+    print(f"IMG {n+1}: {image_files[n]}")
+    try:
+        functs.pdr_to_fits_images(image_files[n], camera_filter, camera)
+    except Exception as e:
+        print(f"{e}\n")
+        failed_converted_files.append(str(image_files[n]))
+        total_failed_conversion += 1
+    else:
+        functs.plot_fits_images(image_files[n], camera_filter, camera)
+        functs.plot_number_of_rejected_pixels(image_files[n], camera_filter, camera)
+        total_converted_images += 1
+    
+print(f"{camera_filter} {camera} Camera images converted successfully!\n")
+
+print(f"Total number of image files: {image_files_length}")
+print(f"Number of successfully converted images: {total_converted_images}")
+print(f"Number of failed conversion: {total_failed_conversion}")
+print(f"Failed converted files: {failed_converted_files}")
+
