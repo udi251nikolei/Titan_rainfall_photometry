f1d05feb142b8ab528804ea82f79c2ba9ecc29c3
Nikolei <113668786+udi251nikolei@users.noreply.github.com>
2026-02-06 16:25:33 +0100

    Update single_image.py
    Seeing the curve-of-growth to changing the aperture size

--- single_image.py
+++ single_image.py
@@ -1,57 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 
 import functs
 
-#image_file = "N1642241415_1_CALIB"
-#satellite_distance = 1212494.476 * 2
-#exposure_time = 5.6
 
-#image_file = "N1577095185_1_CALIB"
-#satellite_distance = 1283698.306
-#exposure_time = 8.2
-
-#image_file = "W1624421052_1_CALIB"
-#satellite_distance = 177548.592
-#exposure_time = 0.82
-
-image_file = "W1683616251_1_CALIB"
-satellite_distance = 150076.613
+image_files = "W1683616251_1_CALIB"
+planet_cassini_distance = 150076.613
+image_gain = "29"
 exposure_time = 0.82
-gain = 29
 
-# !!! Check folder names
 camera_filter = "CB2"
 camera = "WAC" # !!! Note: always capitalize
 
-pixel_angular_size = 59.749e-6 #rad per pix for Wide angle
-#pixel_angular_size = 5.9907e-6 #rad per pix for Narrow angle
+pixel_angular_size = 59.749e-6 #rad per pix
+#pixel_angular_size = 5.9907e-6 #rad per pix
 
-fields = ['Image file', 'Satellite distance [km]', 'Exposure time [s]', 'Gain [e per DN]',
-          'Aperture count [DN]', 'Aperture area', 'Annulus radius', 'Sky median [DN]',
-          'Aperture area sky count [DN]', 'Source count [DN]', 'Source intenisty [e per s per m2]']
 
-with open("test_output.csv", 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(fields)
+xaxis_aperture = "improved"
+yaxis_aperture = "improved"
+annulus_width = 15
 
-image_data = functs.get_image_data(image_file, camera_filter, camera)
+#--------------------------------------------------------
+
+if (image_gain == '95'):
+    gain = 95
+    satellite_distance = planet_cassini_distance * 2
+elif (image_gain == '29'):
+    gain = 29
+    satellite_distance = planet_cassini_distance
+else:
+    print("Found image with different gain number other than 29 or 95")   
+        
+    gain = int(image_gain)
+    satellite_distance = planet_cassini_distance
 
 try:
-    aperture, annulus = functs.get_titan_aperture(image_data, satellite_distance, pixel_angular_size)
-    #plt.savefig(f"Titan_images/{camera_filter}_{camera}/apertures/{image_files[n]}_cumulative_aperture.png", dpi = 120, bbox_inches='tight')
-    plt.show()
-except:
-    print("Titan is outside of the CCD image. Cannot produce an aperture for this image and proceed to image photometry.\n")
-    #plt.savefig(f"Titan_images/{camera_filter}_{camera}/apertures/{image_files[n]}_cumulative_aperture.png", dpi = 120, bbox_inches='tight')
-    plt.show()
-else:
-    print("Titan found inside the CCD image.\n")
-    photometric_data = functs.image_photometry(image_data, image_file, satellite_distance, exposure_time, gain, aperture, annulus)
-    photometric_data = np.array([photometric_data])
+    image_data = functs.get_image_data(image_files, camera_filter, camera)
+except Exception as e:
+    print(f"{e}\n")
     
-    with open("test_output.csv", 'a') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(photometric_data)
-    
+image_size = image_data.shape[0]
+if (image_size == 254):
+    satellite_distance = planet_cassini_distance * 4
+
+range_of_aperture_sizes = [0] #np.arange(-50, 50, 5)
+aperture_sizes = []
+aperture_counts = []
+sky_medians = []
+source_counts = []
+
+for i in range(len(range_of_aperture_sizes)):
+    try:
+        xaxis_shift_factor = 0.6
+        yaxis_shift_factor = 1
+        aperture, annulus = functs.get_titan_aperture(image_data, 'None', satellite_distance, pixel_angular_size, 
+                                                      xaxis_shift_factor, yaxis_shift_factor,
+                                                      xaxis_aperture, yaxis_aperture, 
+                                                      0, annulus_width)
+        
+        print(f'Aperture size: {range_of_aperture_sizes[i]}')
+        
+    except Exception as e:
+        print(f"{e}")
+        plt.show()
+        break
+        
+    else:
+        plt.show()
+        
+        aperture_count, sky_median, source_count = functs.image_photometry(image_data, aperture, annulus)
+        aperture_counts.append(aperture_count)
+        sky_medians.append(sky_median)
+        source_counts.append(source_count)
+        aperture_sizes.append(0)
+        
+        print(f'Source count w/out sky: {source_count}\n')
+        
+        
+plt.plot(aperture_sizes, aperture_counts, marker='o')
+plt.title('Aperture counts')
+plt.show()
+
+plt.plot(aperture_sizes, sky_medians, marker='o')
+plt.title('Sky median')
+plt.show()
+
+plt.plot(aperture_sizes, source_counts, marker='o')
+plt.title('Source counts')
+plt.show()
